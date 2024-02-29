ccfbc3d044d6fdf95d3651dcf0dc11cf570ad87b
dongngockhanh <dnkhanh.12a3@gmail.com>
2024-02-29 23:06:25 +0700

    tách header và footer ra khỏi home

--- FE/shopapp-angular/src/app/app.module.ts
+++ FE/shopapp-angular/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 // import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     // AppComponent
   
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
