6e5b201e68be00dfa363b696266abc2a99a050a3
dongngockhanh <dnkhanh.12a3@gmail.com>
2024-02-29 15:50:29 +0700

    push FE

---
+++ FE/shopapp-angular/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+// import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    // AppComponent
+  
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+   bootstrap: [/*AppComponent*/HomeComponent]
+})
+export class AppModule { }
