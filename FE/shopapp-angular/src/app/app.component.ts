25bef717bd83c4e439866ecd7b19de3bf5b64ca0
dongngockhanh <dnkhanh.12a3@gmail.com>
2024-03-02 16:20:07 +0700

    fixed page refesh of dark mode feature

--- FE/shopapp-angular/src/app/app.component.ts
+++ FE/shopapp-angular/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,Renderer2,Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'ShopApp-Angular';
+
+  // code under this line is for fixing page refresh issue of dark mode
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {}
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.renderer.setStyle(this.document.body, 'transition', 'all 0.3s linear');
+    }, 0);
+    setTimeout(() => {
+      this.renderer.addClass(this.document.body, 'transition');
+    }, 0);
+  }
 }
--- FE/shopapp-angular/src/main.ts
+++ FE/shopapp-angular/src/main.ts
@@ -4,10 +4,4 @@ import { AppModule } from './app/app.module';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-.then(ref => {
-  if ((window as any)['ngRef']) {
-    (window as any)['ngRef'].destroy();
-  }
-  (window as any)['ngRef'] = ref;
-})
 .catch(err => console.error(err));
